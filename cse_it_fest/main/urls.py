1a7d5f91759d5adc43be951d9f968d2b1d5940bb
Sanzid <55673037+sanzid-olioul@users.noreply.github.com>
2023-02-10 22:00:28 +0600

    Delete cse_it_fest directory

--- cse_it_fest/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Register,List
-urlpatterns = [
-    path('', Register.as_view(),name = 'register'),
-    path('list', List.as_view(),name = 'list'),
-    path('list/<int:roll_no>', List.as_view(),name = 'list-search'),
-]
